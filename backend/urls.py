6a2f36bd82ad018ab371bdbccf2ac963f6870f39
Elambarathi k <156106606+Elambarathi@users.noreply.github.com>
2024-09-12 02:54:52 +0530

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from products.views import ProductViewSet
+from products.views import ProductDeleteView
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
@@ -10,4 +11,6 @@ router.register(r'products', ProductViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/products/<int:pk>/', ProductDeleteView.as_view(), name='product-delete'),
+    
 ]
